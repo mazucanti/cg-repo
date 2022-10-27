b580c1cebe835e9fcdfbca47964c3cf76934367d
mazucanti <daniel.mazucanti.ismart@gmail.com>
2022-10-27 01:54:32 -0300

    quadrado colorido

---
+++ vshader1.glsl
@@ -0,0 +1,12 @@
+#version 400
+
+layout ( location = 0) in vec4 vPosition;
+layout ( location = 1) in vec4 vColors;
+out vec4 v2fcolor;
+
+void main ()
+{
+gl_Position = vPosition ; //gl_position -- variavel built-in
+
+v2fcolor = vColors ;
+}
